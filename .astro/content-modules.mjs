363d4d304727f5b601a001f89a61f9402ee913bf
Waleed H.Khan <waleed.hussain@preis.de>
2025-02-11 16:59:19 +0100

    Add new post

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
-["src/content/posts/ 2025-01-19-sublime-text.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F+2025-01-19-sublime-text.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/ 2025-01-19-sublime-text.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F+2025-01-19-sublime-text.mdx&astroContentModuleFlag=true")],
+["src/content/posts/ 2025-02-11-accessibility.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F+2025-02-11-accessibility.mdx&astroContentModuleFlag=true")]]);
 		
