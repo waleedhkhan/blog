bed5dcd19201ed0476cfac898e6802e0527b929d
Waleed H.Khan <hello@waleed.de>
2025-02-14 13:21:18 +0100

    Added a new post

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,6 @@
 
 export default new Map([
 ["src/content/posts/ 2025-01-19-sublime-text.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F+2025-01-19-sublime-text.mdx&astroContentModuleFlag=true")],
+["src/content/posts/2025-02-14-spotify.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F2025-02-14-spotify.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/ 2025-02-11-accessibility.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F+2025-02-11-accessibility.mdx&astroContentModuleFlag=true")]]);
 		
