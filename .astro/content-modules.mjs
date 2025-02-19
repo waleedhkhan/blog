efa03fb76967ff606adeb666e88f901935fbff06
Waleed H.Khan <hello@waleed.de>
2025-02-19 15:03:09 +0100

    Adding astro-umami-analytics

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
 ["src/content/posts/ sublime-text.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F+sublime-text.mdx&astroContentModuleFlag=true")],
-["src/content/posts/spotify.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fspotify.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/accessibility.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Faccessibility.mdx&astroContentModuleFlag=true")],
-["src/content/posts/effective-prompts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Feffective-prompts.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/effective-prompts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Feffective-prompts.mdx&astroContentModuleFlag=true")],
+["src/content/posts/spotify.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fspotify.mdx&astroContentModuleFlag=true")]]);
 		
