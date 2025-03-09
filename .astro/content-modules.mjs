421d2cfe06c3a424ab8716a9313646e111f5e1af
Waleed H.Khan <info@waleed.de>
2025-03-09 17:31:30 +0100

    Cleanup styles for Card and Navigation components; update BookmarkFeed layout and enhance dark mode support in CSS.

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,7 +2,7 @@
 export default new Map([
 ["src/content/posts/ sublime-text.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2F+sublime-text.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/accessibility.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Faccessibility.mdx&astroContentModuleFlag=true")],
-["src/content/posts/effective-prompts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Feffective-prompts.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/future-of-jobs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ffuture-of-jobs.mdx&astroContentModuleFlag=true")],
+["src/content/posts/effective-prompts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Feffective-prompts.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/spotify.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fspotify.mdx&astroContentModuleFlag=true")]]);
 		
